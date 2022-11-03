cc3766d12db7cace00a959b9acb5922b019de3df
unknown <SKA@lap-lenovo0640.ska.com.br>
2022-11-03 15:49:42 -0300

    alterado arquivo de configuração do projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  "reporter": "mochawesome",
+  "reporterOptions": {
+  "reportDir": "cypress/report/mochawesome-report",
+  "overwrite": true,
+  "html": true,
+  "json": true,
+  "timestamp": "mmddyyyy_HHMMss",
+  }
   projectId: 'i9wr4r',
   e2e: {
     setupNodeEvents(on, config) {
-      "reporter": "mochawesome",
-      "reporterOptions": {
-      "reportDir": "cypress/report/mochawesome-report",
-      "overwrite": true,
-      "html": true,
-      "json": true,
-      "timestamp": "mmddyyyy_HHMMss"
-    }
     },
   },
 });
